39310cbbe7143e320ac2ea7ab0ba356877e54be1
Ramy Kelenda <mara@Ramys-MacBook-Air.local>
2024-08-06 20:46:41 +0100

    start function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,114 @@
 const quizInfo = [
-  {
-      question: "1.What country has the highest life expenctancy?",
-      a: "A.Spain",
-      b: "B.Brasil",
-      c: "C.Hong Kong",
-      d: "C.Indian",
-      correct: "c",
-  },
-  {
-      question: "2.What is the most common surname in the united states?",
-      a: "A.Smith",
-      b: "B.John",
-      c: "C.Dev",
-      d: "D.Jack",
-      correct: "a",
-  },
-  {
-      question: "3.Who was the Ancient Greek God of the sun?",
-      a: "A.Deus",
-      b: "B.Apollo",
-      c: "C.Abed",
-      d: "D.Apolos",
-      correct: "b",
-  },
-  {
-      question: "4.What Car manufacture had highest revenue in 2020?",
-      a: "A.Volkswagen",
-      b: "B.Volvo",
-      c: "C.Toyota",
-      d: "D.None of above",
-      correct: "a",
-  },
-  {
-      question: "5.What company was originally called cadable?",
-      a: "A.Amazon",
-      b: "B.Samsung",
-      c: "C.DHL",
-      d: "D.UPC",
-      correct: "a",
-  },
-  {
-      question: "6.What country drinks most coffee per capita?",
-      a: "A.France",
-      b: "B.Italy",
-      c: "C.Ireland",
-      d: "D.Finland",
-      correct: "d",
-  },
-  {
-      question: "7.What city is known as The Eternal city?",
-      a: "A.Istabul",
-      b: "B.London",
-      c: "C.Rome",
-      d: "D.Paris",
-      correct: "c",
-  },
-  {
-      question: "8.What is the name of the capital city of the republic democratic of congo?",
-      a: "A.Lubumbashi",
-      b: "B.Kivu",
-      c: "C.Kinshasa",
-      d: "D.Bas-congo",
-      correct: "c",
-  },
-  {
-      question: "9.What country has won the most world cups?",
-      a: "A.Germany",
-      b: "B.England",
-      c: "C.Brasil",
-      d: "A.Spain",
-      correct: "c",
-  },
-  {
-      question: "10.December 26th is Known by what name in Ireland?",
-      a: "A.St Patrick's day",
-      b: "B.St Stephen's day",
-      c: "C.Chrismas day",
-      d: "D.None of above",
-      correct: "b",
-  },
+    {
+        question: "1.What country has the highest life expenctancy?",
+        a: "A.Spain",
+        b: "B.Brasil",
+        c: "C.Hong Kong",
+        d: "C.Indian",
+        correct: "c",
+    },
+    {
+        question: "2.What is the most common surname in the united states?",
+        a: "A.Smith",
+        b: "B.John",
+        c: "C.Dev",
+        d: "D.Jack",
+        correct: "a",
+    },
+    {
+        question: "3.Who was the Ancient Greek God of the sun?",
+        a: "A.Deus",
+        b: "B.Apollo",
+        c: "C.Abed",
+        d: "D.Apolos",
+        correct: "b",
+    },
+    {
+        question: "4.What Car manufacture had highest revenue in 2020?",
+        a: "A.Volkswagen",
+        b: "B.Volvo",
+        c: "C.Toyota",
+        d: "D.None of above",
+        correct: "a",
+    },
+    {
+        question: "5.What company was originally called cadable?",
+        a: "A.Amazon",
+        b: "B.Samsung",
+        c: "C.DHL",
+        d: "D.UPC",
+        correct: "a",
+    },
+    {
+        question: "6.What country drinks most coffee per capita?",
+        a: "A.France",
+        b: "B.Italy",
+        c: "C.Ireland",
+        d: "D.Finland",
+        correct: "d",
+    },
+    {
+        question: "7.What city is known as The Eternal city?",
+        a: "A.Istabul",
+        b: "B.London",
+        c: "C.Rome",
+        d: "D.Paris",
+        correct: "c",
+    },
+    {
+        question: "8.What is the name of the capital city of the republic democratic of congo?",
+        a: "A.Lubumbashi",
+        b: "B.Kivu",
+        c: "C.Kinshasa",
+        d: "D.Bas-congo",
+        correct: "c",
+    },
+    {
+        question: "9.What country has won the most world cups?",
+        a: "A.Germany",
+        b: "B.England",
+        c: "C.Brasil",
+        d: "A.Spain",
+        correct: "c",
+    },
+    {
+        question: "10.December 26th is Known by what name in Ireland?",
+        a: "A.St Patrick's day",
+        b: "B.St Stephen's day",
+        c: "C.Chrismas day",
+        d: "D.None of above",
+        correct: "b",
+    },
+ 
+ ];
 
-];
+const quiz = document.getElementById('quiz')
+const answerElements = document.querySelectorAll('.answer')
+const questionElement = document.getElementById('question')
+const first = document.getElementById('first')
+const second = document.getElementById('second')
+const third = document.getElementById('third')
+const fourth = document.getElementById('fourth')
+let newQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    deselectAnswers()
+    const newQuizInfo = quizInfo[newQuiz]
+    questionElement.innerText = newQuizInfo.question
+    first.innerText.newQuizInfo.a
+    second.innerText.newQuizInfo.b
+    third.innerText.newQuizInfo.c
+    fourth.innerText.newQuizInfo.d
+}
+
+function startQuiz() {
+    let welcomeScreen = document.getElementsByClassName("welcome-quiz");
+    let playArea = document.getElementsByClassName("rkQuiz-area");
+  
+    welcomeScreen.style.display = none;
+    playArea.style.display = block; 
+  }
+  
